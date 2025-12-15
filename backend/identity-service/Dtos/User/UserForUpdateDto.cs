4c6ee52feaee72af698347f2f523bd1290f4bab4
Isaac Valdez <jvaldezb@gmail.com>
2025-12-15 14:58:31 -0500

    feat(User-update): change Dto

--- backend/identity-service/Dtos/User/UserForUpdateDto.cs
+++ backend/identity-service/Dtos/User/UserForUpdateDto.cs
@@ -7,7 +7,7 @@ public class UserForUpdateDto
     public required string FullName { get; set; }
     public required string UserName { get; set; }
     public required string Email { get; set; }
-    public required string Password { get; set; }
+    public string? Password { get; set; }
     public required string DocumentType { get; set; }
     public required string DocumentNumber { get; set; }
     public List<Guid> RoleIds { get; set; } = new();
